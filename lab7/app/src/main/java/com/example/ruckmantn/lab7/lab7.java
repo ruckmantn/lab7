47dda5d8532a0854f875d030f13e436426a784a5
ruckmantn <46693794+ruckmantn@users.noreply.github.com>
2019-04-15 17:54:49 -0400

    Add files via upload

---
+++ lab7/app/src/main/java/com/example/ruckmantn/lab7/lab7.java
@@ -0,0 +1,13 @@
+package com.example.ruckmantn.lab7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class lab7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab7);
+    }
+}
